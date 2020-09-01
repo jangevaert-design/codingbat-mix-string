46f519237ebbad126033f12de6e13824063612bf
Jan Gevaert <jan.gevaert@gmail.com>
2020-09-01 06:36:23 -0600

    avoided string concatenation in a for loop by using StringBuilder.

--- src/mixString.java
+++ src/mixString.java
@@ -3,18 +3,18 @@ public class mixString {
     int lena = a.length();
     int lenb = b.length();
     int max = Math.max(lena, lenb);
-    String result = "";
+    StringBuilder result = new StringBuilder();
 
     for (int i = 0; i < max; i++) {
       if (i <= lena - 1) {
-        result += a.substring(i, i + 1);
+        result.append(a.charAt(i));
       }
       if (i <= lenb - 1) {
-        result += b.substring(i, i + 1);
+        result.append(b.charAt(i));
       }
 
     }
 
-    return result;
+    return result.toString();
   }
 }
